f22144e2d40f3850bae93852dc0ba9c309aac359
Alexey-Gorulev <lex-good-go@yandex.ru>
2019-11-02 20:32:11 +0300

    fix

--- index.php
+++ index.php
@@ -8,24 +8,7 @@ and the most populous metropolitan area in the world.
 It is the seat of the Japanese government and the Imperial Palace,
 and the home of the Japanese Imperial Family.</p>
 
-<?php  /* If the error is due to calling mysqli in a class */
-
-    class dbconnect extends mysqli{
-
-        private $host = 'localhost';
-        private $user = 'root';
-        private $pass = '123456';
-        private $db = 'db';
-
-        public function __construct() {
-            parent::__construct($this->host, $this->user, $this->pass, $this->db);
-
-            if (mysqli_connect_error()) {
-                die('Connect Error (' . mysqli_connect_errno() . ') ' .
-                    mysqli_connect_error());
-            }
-        }
-    }
+<?php  
 
 phpinfo();
 
